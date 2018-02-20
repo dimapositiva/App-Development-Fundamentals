c3821db530085095cf8a5944481d754c19309067
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-02-20 11:46:27 +0200

    "Task #12 of lesson 14 was solve."

---
+++ lesson 14 (js - basics)/script_14-12.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let nameList = [];
+nameList = [3, 'string', true, null]; //(4) [3, "string", true, null]
+
+nameList.length; //4
+
+nameList[4] = prompt('Enter some value', ''); //"Bro"
+
+nameList; //(5) [3, "string", true, null, "Bro"]
+
+console.log(nameList[4]); //"Bro"
