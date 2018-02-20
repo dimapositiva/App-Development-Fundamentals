fd81c03146a2499cb1efada3831523606960d5da
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-02-20 12:57:21 +0200

    "Task #3 of lesson 15 was solve."

---
+++ lesson 15 (js - conditional constructions, cycles, functions)/script_15-3.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const differentNumberChecker = (a, b, c, d) => {
+  if (a !== b && a !== c && a !== d) {
+    console.log('Index of different number is #1.');
+  } else if (b !== a && b !== c && b !== d) {
+    console.log('Index of different number is #2.');
+  } else if (c !== a && c !== b && c !== d) {
+    console.log('Index of different number is #3.');
+  } else if (d !== a && d !== b && d !== c) {
+      console.log('Index of different number is #4.');
+    } else {
+    console.log('Something wrong. Maybe all number equel.')
+  }
+}
