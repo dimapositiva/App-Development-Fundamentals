c3643aea01db25854ff5dcb03be06872eccc8122
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-02-21 16:12:23 +0200

    "Task #7 of lesson 15 was solve."

---
+++ lesson 15 (js - conditional constructions, cycles, functions)/script_15-7.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const getSummNumberInterval = (a, b) => {
+  if (a < b) {
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+    console.log(sum);
+  } else {
+    console.log('Error! Second argument must be bigger then first.')
+  }
+}
