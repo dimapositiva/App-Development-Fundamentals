5c9080f16f4dc585dd4e2e96474f54327d0455c3
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-02-21 15:58:59 +0200

    "Task #6 of lesson 15 was solve."

---
+++ lesson 15 (js - conditional constructions, cycles, functions)/script_15-6.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const getSingleDigit = n => {
+  if (n > 0) {
+    while (n > 1) {
+      let a;
+      a = n % 10;
+      console.log(parseInt(a));
+      n /= 10;
+    }
+  } else {
+    console.log('Error! Please enter positive number.');
+  }
+}
