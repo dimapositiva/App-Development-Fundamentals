163f3a9e504cf9caeb191053b7432f4e50efcd5b
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-02-22 17:23:19 +0200

    "Code refactoring script_15-15."

--- lesson 15 (js - conditional constructions, cycles, functions)/script_15-15.js
+++ lesson 15 (js - conditional constructions, cycles, functions)/script_15-15.js
@@ -2,15 +2,14 @@
 
 const digitN = (k, n) => {
   if (k > 0 && n > 0) {
-    let num = k;
     for (let i = 0; i < (n - 1); i++) {
-      num /= 10;
+      k /= 10;
     }
 
-    if (num < 1) {
+    if (k < 1) {
       console.log('Error! Value "n" more than digits in "k".');
     } else {
-      let res = num % 10;
+      let res = k % 10;
       console.log(parseInt(res));
     }
 
