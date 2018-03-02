1c2c0f9e678b5634b114b817f62346ab21ae0d92
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-02 13:40:46 +0200

    "Task #5 of lesson 17 was solve."

---
+++ lesson 17 (js - OOD, objects)/script_17-5.js
@@ -0,0 +1,17 @@
+let coders = {
+  'John': {
+    'age': 28,
+    'experience': 1,
+    'languages': ['Java', 'JavaScript', 'SQL']
+  },
+  'Bill': {
+    'age': 30,
+    'experience': 5,
+    'languages': ['HTML', 'CSS', 'JavaScript']
+  },
+  'Mike': {
+    'age': 32,
+    'experience': 4,
+    'languages': ['Python', 'SQL']
+  }
+};
