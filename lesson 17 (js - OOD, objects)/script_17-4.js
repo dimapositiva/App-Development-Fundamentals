45f0dacd4ee1bacf5d2ce99fc820ca321c1a4db6
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-02 13:32:30 +0200

    "Task #4 of lesson 17 was solve."

---
+++ lesson 17 (js - OOD, objects)/script_17-4.js
@@ -0,0 +1,61 @@
+let player = {
+  'tracks': [],
+  'currentTrack': 0,
+  'volume': 95,
+  'maxVolume': 100,
+  'minVolume': 0,
+  addTrack: (track) => {
+    player.tracks.push(track);
+  },
+  deleteTrack: (track) => {
+    let i = player.tracks.indexOf(track);
+    player.tracks.splice(i, 1);
+  },
+  displayAllTracks: () => {
+    for (let i = 0; i < player.tracks.length; i++) {
+      console.log('Track ' + (i + 1) + '. ' + player.tracks[i]);
+    }
+  },
+  displayCurrentTrack: () => {
+    console.log(player.tracks[player.currentTrack]);
+  },
+  nextTrack: () => {
+    if (player.currentTrack < player.tracks.length - 1) {
+      player.currentTrack += 1;
+    } else {
+      return player.currentTrack;
+    }
+  },
+  previousTrack: () => {
+    if (player.currentTrack !== 0) {
+      player.currentTrack -= 1;
+    } else {
+      return player.currentTrack;
+    }
+  },
+  random: () => {
+    let j = parseInt(Math.random()*100);
+    if (j <= player.tracks.length - 1 && j !== player.currentTrack) {
+      player.currentTrack = j;
+    } else {
+      return player.random();
+    }
+  },
+  increaseVolume: () => {
+    if (player.volume < player.maxVolume) {
+      player.volume += 5;
+    } else {
+      return player.volume;
+    }
+  },
+  decreaseVolume: () => {
+    if (player.volume > player.minVolume) {
+      player.volume -= 5;
+    } else {
+      return player.volume;
+    }
+  },
+  displayVolume: () => {
+    console.log('Current volume is ' + player.volume);
+  }
+}
