3ef2787494fa1f61acd2ba4868c293239ff5cb52
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-09 11:32:30 +0200

    "Code refactoring script_20-2.js"

--- lesson 20 (js - OOD, prototypes)/script_20-2.js
+++ lesson 20 (js - OOD, prototypes)/script_20-2.js
@@ -1,35 +1,38 @@
 function Square() {
-    this._side = 0;
-    this._type = 'square';
-    this._perimeter = 0;
-    this.setSide = (num) => {
-        if (!isNaN(num)) {
-            this._side = num;
-            return this._side;
-        } else {
+  this.side = 0;
+  this.type = 'square';
+  this.perimeter = 0;
+  this.setSide = (num) => {
+    if (!isNaN(num)) {
+      this.side = num;
+      return this.side;
+    } else {
         console.log('Argument must be a number.');
-        }
-    };
-    this.getPerimeter = () => {
-        this._perimeter = 2 * this._side;
-        return this._perimeter;
-    };
-    this.info = () => {
-        console.log('Side is: ' + this._side + '\n' +
-            'type of figure: ' + this._type + '\n' +
-            'perimeter is: ' + this._perimeter);
-    };
+      }
+  };
+  this.getPerimeter = () => {
+    this.perimeter = 2 * this.side;
+    return this.perimeter;
+  };
+  this.info = () => {
+    console.log('Side is: ' + this.side + '\n' +
+                'type of figure: ' + this.type + '\n' +
+                'perimeter is: ' + this.perimeter);
+  };
 }
 
 function Cube() {
-    Square.call(this);
-    this._type = 'cube';
-    this.getPerimeter = () => {
-        this._perimeter = 12 * this._side;
-        return this._perimeter;
-    };
-    this.getPerimeter2 = () => {
-        this._perimeter = 2 * this._side;
-        return 6 * this._perimeter;
-    };
+  Square.call(this);
+  this.type = 'cube';
+  this.getPerimeter = () => {
+    this.perimeter = 12 * this.side;
+    return this.perimeter;
+  };
+
+    //Override with polymorphism
+  let perim = this.getPerimeter.bind(this);
+  this.getPerimeter2 = () => {
+    perim.call(this);
+    return 6 * this.perimeter;
+  };
 }
