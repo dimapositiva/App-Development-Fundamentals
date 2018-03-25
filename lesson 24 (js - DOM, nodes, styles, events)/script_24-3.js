5dbd6b20cb7564f45af7d51c8db8ac172d356356
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-25 21:11:22 +0300

    "Bug fixing."

--- lesson 24 (js - DOM, nodes, styles, events)/script_24-3.js
+++ lesson 24 (js - DOM, nodes, styles, events)/script_24-3.js
@@ -2,7 +2,7 @@
 let btn = document.createElement("label");
 
 // insert new inpup in label element
-btn.innerHTML = '<input type="button" value="submit" method="gpost" />';
+btn.innerHTML = '<input type="button" value="submit" method="post" />';
 
 // insert in form element new label element
 document.body.children[0].appendChild(btn);
