e5794890475373d2ab596e97b5d259d2ee666c6d
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-25 21:07:20 +0300

    "Task #3 of lesson 24 was solve."

---
+++ lesson 24 (js - DOM, nodes, styles, events)/script_24-3.js
@@ -0,0 +1,8 @@
+// craete label element
+let btn = document.createElement("label");
+
+// insert new inpup in label element
+btn.innerHTML = '<input type="button" value="submit" method="gpost" />';
+
+// insert in form element new label element
+document.body.children[0].appendChild(btn);
