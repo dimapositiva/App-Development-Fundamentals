5fb6bad5085d455865b011a3e4ca2fa1c6d905e7
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-25 21:18:36 +0300

    "Replace from input (type = button) to button element in .js file."

--- lesson 24 (js - DOM, nodes, styles, events)/script_24-3.js
+++ lesson 24 (js - DOM, nodes, styles, events)/script_24-3.js
@@ -2,7 +2,7 @@
 let btn = document.createElement("label");
 
 // insert new inpup in label element
-btn.innerHTML = '<input type="button" value="submit" method="post" />';
+btn.innerHTML = '<button type="submit" formmethod="post">submit</button>';
 
 // insert in form element new label element
 document.body.children[0].appendChild(btn);
