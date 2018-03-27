5547f4ff30103859f098d3089bdc763ab759bb19
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-27 11:46:56 +0300

    "Delete comments."

--- lesson 25 (js - ES6, destructuring, arrow functions, strings)/script_25-7.js
+++ lesson 25 (js - ES6, destructuring, arrow functions, strings)/script_25-7.js
@@ -3,5 +3,5 @@ let names = {
    second: "Sam",
    third: "Ray"
 };
-console.log(`First: -${names.first}-, Second: -${names.second}-, Third: -${names.third}-`); // "Первый: -Tom-, Второй - "Sam", Третий => 'Ray'"
-// Допишите код используя интерполяцию, чтобы в консоли браузера появилась строка, которая написана в комментариях.
+
+console.log(`First: -${names.first}-, Second: -${names.second}-, Third: -${names.third}-`); // "First: -Tom-, Second: -Sam-, Third: -Ray-"
