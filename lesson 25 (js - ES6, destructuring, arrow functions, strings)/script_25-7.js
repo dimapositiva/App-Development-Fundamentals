d836efa471a288d7434e315104e01ef8a4b86a36
Dmytro Soloviov <d.v.solovyov1@gmail.com>
2018-03-27 11:46:08 +0300

    "Task #7 of lesson 25 was solve."

---
+++ lesson 25 (js - ES6, destructuring, arrow functions, strings)/script_25-7.js
@@ -0,0 +1,7 @@
+let names = {
+   first: "Tom",
+   second: "Sam",
+   third: "Ray"
+};
+console.log(`First: -${names.first}-, Second: -${names.second}-, Third: -${names.third}-`); // "Первый: -Tom-, Второй - "Sam", Третий => 'Ray'"
+// Допишите код используя интерполяцию, чтобы в консоли браузера появилась строка, которая написана в комментариях.
